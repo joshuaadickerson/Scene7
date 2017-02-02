5f2192c0b06802cc31fc985458e7f0efc4f5ab5f
Joshua Dickereson <joshuad@bakerdist.com>
2017-02-02 13:29:13 -0500

    Implement Unsharp-Mask

--- src/Commands/UnsharpMask.php
+++ src/Commands/UnsharpMask.php
@@ -6,11 +6,40 @@ trait UnsharpMask
 {
     public function setUnsharpMask($amount, $radius = null, $threshold = null, $monochrome = null)
     {
-        if (!in_array($use, $this->getAllowedMaskUses())) {
-            throw new \InvalidArgumentException('Invalid mask usage');
+        $amount = (float) $amount;
+        if ($amount < 0 || $amount > 5) {
+            throw new \InvalidArgumentException('Invalid amount');
         }
 
-        $this->addCommand(['maskUse' => $use]);
+        if ($radius !== null) {
+            $radius = (float) $radius;
+            if ($radius < 0 || $radius > 250) {
+                throw new \InvalidArgumentException('Invalid radius');
+            }
+        }
+
+        if ($threshold !== null) {
+            $threshold = (float) $threshold;
+            if ($threshold < 0 || $threshold > 255) {
+                throw new \InvalidArgumentException('Invalid threshold');
+            }
+        }
+
+        $mask = $amount;
+
+        if ($radius !== null || $threshold !== null || $monochrome !== null) {
+            $mask .= ',' . $radius;
+        }
+
+        if ($threshold !== null || $monochrome != null) {
+            $mask .= ',' . $threshold;
+        }
+
+        if ($monochrome !== null) {
+            $mask .= ',' . (int) (bool) $monochrome;
+        }
+
+        $this->addCommand(['op_usm' => $mask]);
         return $this;
     }
 }
